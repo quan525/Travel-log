cb723e370972452ba948065ff6292a7dd0ace7be
Quan <quanchu64@gmail.com>
2026-07-14 14:58:16 +0700

    update auth button

--- store/auth.ts
+++ store/auth.ts
@@ -1,42 +1,28 @@
-import { createAuthClient } from "better-auth/vue";
+import { createAuthClient } from "better-auth/vue"
 
-const authClient = createAuthClient();
+const authClient = createAuthClient()
 
 export const useAuthStore = defineStore("useAuthStore", () => {
-  const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
+  const session = authClient.useSession()
 
   async function init() {
-    const data = await authClient.useSession(useFetch);
-    session.value = data;
+    await session.value.refetch()
   }
 
-  const user = computed(() => session.value?.data?.user);
-  const loading = computed(() => session.value?.isPending);
+  const user = computed(() => session.value.data?.user ?? null)
+  const loading = computed(() => session.value.isPending)
 
   async function signIn() {
-    const { csrf } = useCsrf();
-    const headers = new Headers();
-    headers.append("csrf-token", csrf);
     await authClient.signIn.social({
       provider: "github",
       callbackURL: "/dashboard",
       errorCallbackURL: "/error",
-      fetchOptions: {
-        headers,
-      },
-    });
+    })
   }
 
   async function signOut() {
-    const { csrf } = useCsrf();
-    const headers = new Headers();
-    headers.append("csrf-token", csrf);
-    await authClient.signOut({
-      fetchOptions: {
-        headers,
-      },
-    });
-    navigateTo("/");
+    await authClient.signOut()
+    await navigateTo("/")
   }
 
   return {
@@ -45,5 +31,5 @@ export const useAuthStore = defineStore("useAuthStore", () => {
     signIn,
     signOut,
     user,
-  };
-});
+  }
+})
