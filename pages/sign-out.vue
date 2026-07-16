04edae011f2958e87e73792f957efebd39810320
Quan <quanchu64@gmail.com>
2026-07-16 15:18:16 +0700

    update auth state

---
+++ pages/sign-out.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useAuthStore } from '~/store/auth'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.signOut()
+})
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col ">
+    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
+  </div>
+</template>
