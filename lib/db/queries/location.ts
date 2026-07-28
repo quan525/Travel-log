26ccb1de3b824add698383e0010788613550d20c
quan525 <quanchu64@gmail.com>
2026-07-28 22:07:58 +0700

    add location query

---
+++ lib/db/queries/location.ts
@@ -0,0 +1,82 @@
+import type { InsertLocation } from "../schema";
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import db from "..";
+import { location } from "../schema";
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
+
+export async function findLocations(userId: number) {
+  return db.query.location.findMany({
+    where: eq(location.userId, userId),
+  });
+}
+
+export async function findLocationByName(existing: InsertLocation, userId: number) {
+  return db.query.location.findFirst({
+    where: and(
+      eq(location.name, existing.name),
+      eq(location.userId, userId),
+    ),
+  });
+};
+
+export async function findLocationBySlug(slug: string) {
+  return await db.query.location.findFirst({
+    where:
+      and(
+        eq (location.slug, slug),
+      ),
+  });
+}
+
+export async function findUniqueSlug(slug: string) {
+  let existing = !!(await findLocationBySlug(slug));
+
+  while (existing) {
+    const id = nanoid();
+    const idSlug = `${slug} - ${id}`;
+    existing = !!(await findLocationBySlug(idSlug));
+    if (!existing) {
+      slug = idSlug;
+    }
+  }
+
+  return slug;
+}
+
+export async function insertLocation(
+  insertable: InsertLocation,
+  slug: string,
+  userId: number,
+) {
+  const [created] = await db.insert(location).values({
+    ...insertable,
+    slug,
+    userId,
+  }).returning();
+  return created;
+}
+
+export async function updateLocationBySlug(
+  updates: InsertLocation,
+  slug: string,
+  userId: number,
+) {
+  const [updated] = await db.update(location).set(updates).where(and(
+    eq(location.slug, slug),
+    eq(location.userId, userId),
+  )).returning();
+  return updated;
+}
+
+export async function removeLocationBySlug(
+  slug: string,
+  userId: number,
+) {
+  const [removed] = await db.delete(location).where(and(
+    eq(location.slug, slug),
+    eq(location.userId, userId),
+  )).returning();
+  return removed;
+}
